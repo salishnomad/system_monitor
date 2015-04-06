64f76f08ac3140e5b75bee03daeb05cef216a338
Olivier LeDiouris <olivier.lediouris@oracle.com>
2015-04-06 11:24:54 -0700

    Serious cleanup

--- make.desktop.bat
+++
@@ -1,8 +0,0 @@
-@setlocal
-@echo off
-@set JAVA_HOME=C:\JDev.11.1.1.5.0\jdk160_24
-@set WHERE_IS_ANT=C:\JDev.11.1.1.5.0\jdeveloper\ant\bin
-:: set JAVA_OPTIONS=-Dhttp.proxyHost=www-proxy.us.oracle.com -Dhttp.proxyPort=80
-call %WHERE_IS_ANT%\ant %JAVA_OPTIONS% -buildfile build.master.dektop.xml %*
-:: pause
-@endlocal
--- make.weather.wizard.bat
+++
@@ -1,8 +0,0 @@
-@setlocal
-@echo off
-@set JAVA_HOME=D:\Java\jdk1.7.0_45
-@set WHERE_IS_ANT=D:\Ant.1.8\bin
-:: set JAVA_OPTIONS=-Dhttp.proxyHost=www-proxy.us.oracle.com -Dhttp.proxyPort=80
-call %WHERE_IS_ANT%\ant %JAVA_OPTIONS% -buildfile build.weather.wizard.xml %*
-:: pause
-@endlocal
